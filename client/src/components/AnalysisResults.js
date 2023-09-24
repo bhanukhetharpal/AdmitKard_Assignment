276630cfdd0efe62d05a316552387861be2a0f88
Bhanu Khetharpal <bhanu2paul@gmail.com>
2023-09-24 23:20:26 +0530

    added filter option

--- client/src/components/AnalysisResults.js
+++ client/src/components/AnalysisResults.js
@@ -1,26 +1,49 @@
 import React, { useState } from "react";
 import Card from "react-bootstrap/Card";
+import InputGroup from "react-bootstrap/InputGroup";
+import Form from "react-bootstrap/Form";
 
 function AnalysisResults({ analysisData }) {
+  const [searchKeyword, setSearchKeyword] = useState("");
+  
   if (!analysisData) {
     return null;
   }
   const { top5Pairs = [], top5Words = [] } = analysisData;
 
+  // Filter top words and pairs based on the searchKeyword
+  const filteredTopWords = top5Words.filter((word) =>
+    word.includes(searchKeyword)
+  );
+  const filteredTopPairs = top5Pairs.filter((pair) =>
+    pair.includes(searchKeyword)
+  );
+
   return (
     <div>
       <h1>Text Analysis Results</h1>
+      
+      {/* Keyword search input */}
+      <InputGroup className="mb-3">
+        <Form.Control
+          type="text"
+          placeholder="Search for a keyword"
+          value={searchKeyword}
+          onChange={(e) => setSearchKeyword(e.target.value)}
+        />
+      </InputGroup>
+
       {/* Display top words */}
       <div>
         <h2>Top 5 Mostly Occurring Words</h2>
-        {top5Words.length === 0 ? (
-          <p>No top words found</p>
+        {filteredTopWords.length === 0 ? (
+          <p>No matching top words found</p>
         ) : (
           <div className="cards">
-            {top5Words.map((word) => (
+            {filteredTopWords.map((word, index) => (
               <Card
                 bg="info"
-                key="info"
+                key={index}
                 className="mb-2"
                 style={{ width: "18rem" }}
                 body
@@ -32,17 +55,17 @@ function AnalysisResults({ analysisData }) {
         )}
       </div>
 
-      {/* Display top 5 co-occuring words */}
+      {/* Display top 5 co-occurring words */}
       <div>
         <h2>Top 5 Mostly Occurring Pairs</h2>
-        {top5Pairs.length === 0 ? (
-          <p>No top words found</p>
+        {filteredTopPairs.length === 0 ? (
+          <p>No matching top pairs found</p>
         ) : (
           <div className="cards">
-            {top5Pairs.map((word) => (
+            {filteredTopPairs.map((word, index) => (
               <Card
                 bg="info"
-                key="info"
+                key={index}
                 className="mb-2"
                 style={{ width: "18rem" }}
                 body
